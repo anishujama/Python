9e6f2f8bbfedcad13225e9657321bd1aeb2b0e61
Anishujama khan <anishkh9374@gmail.com>
2025-09-12 00:19:36 +0530

    Done

---
+++ Python problems/day1/1st.py
@@ -0,0 +1,9 @@
+#write a program to display a person's name,age and address in three different lines
+
+name = "john snow"
+age = 23
+adress = "North"
+
+print(name)
+print(age)
+print(adress)
---
+++ Python problems/day1/3rd.py
@@ -0,0 +1,8 @@
+#write a program to convert a float into integer
+
+weight = 55.55
+print(type(weight))
+
+weight = int(weight)
+print(type(weight))
+print(weight)
