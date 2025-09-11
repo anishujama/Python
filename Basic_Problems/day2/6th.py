f1785c08b2f83a3f1899d0a6a70832d8b6b4e825
Anishujama khan <anishkh9374@gmail.com>
2025-09-12 00:22:37 +0530

    Done

---
+++ Basic_Problems/day2/6th.py
@@ -0,0 +1,7 @@
+#write a program to check a number is positive
+
+num = int(input("enter a number here :"))
+if num>=0:
+    print("number is positive")
+else:
+    print("number is negative")
---
+++ Basic_Problems/day4/calculator.py
@@ -0,0 +1,7 @@
+num1 = int(input("enter num1:"))
+num2 = int(input("enter num2:"))
+
+print("the value:",num1,"+",num2,"is:",num1+num2)
+print("the value:",num1,"-",num2,"is:",num1+num2)
+print("the value:",num1,"*",num2,"is:",num1+num2)
+print("the value:",num1,"/",num2,"is:",num1+num2)
